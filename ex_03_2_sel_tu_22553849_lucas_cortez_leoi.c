9e83ab2c984cea25ff304cb5dab6cb6f925c1b77
Lucascortezleoi <lucasleoi2007@gmail.com>
2025-09-23 22:45:10 -0300

    Create ex_03_2_sel_tu_22553849_lucas_cortez_leoi.c

---
+++ ex_03_2_sel_tu_22553849_lucas_cortez_leoi.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <locale.h>
+
+void main(void) {
+	setlocale(LC_ALL, "Portuguese");
+	float valor;
+	
+	printf("\nDigite o valor: ");
+	
+	scanf("%f", &valor);
+	
+	printf("\nValor digitado: %.2f", valor);
+	if (valor > 0) {
+		printf("\nN�mero positivo");
+		printf("\nDobro do valor: %.2f", valor * 2);
+	} else if (valor < 0) {
+		printf("\nN�mero negativo");
+		printf("\nTriplo do valor: %.2f", valor * 3);
+	} else {
+		printf("\nN�mero nulo");
+		printf("\nValor da vari�vel: %.2f", valor);
+	}
+}
