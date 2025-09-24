90ae7f5361edc6268788fbf560fcb9efab00d291
Lucascortezleoi <lucasleoi2007@gmail.com>
2025-09-23 22:43:55 -0300

    Create ex_02_2_sel_tu_22553849_lucas_cortez_leoi.c

---
+++ ex_02_2_sel_tu_22553849_lucas_cortez_leoi.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <locale.h>
+
+void main(void) {
+	setlocale(LC_ALL, "Portuguese");
+	
+	float valor_1, valor_2;
+	
+	printf("\nDigite o primeiro valor: ");
+	
+	scanf("%f", &valor_1);
+
+	printf("\nDigite o segundo valor: ");
+	
+	scanf("%f", &valor_2);
+	
+	if(valor_1 > valor_2) {
+		printf("\nO primeiro valor � maior", valor_1);
+		printf("\nMaior valor: %f", valor_1);
+		printf("\nMenor valor: %f", valor_2);
+	} else if (valor_1 < valor_2) {
+		printf("\nO segundo valor � maior: %f", valor_2);
+		printf("\nMaior valor: %f", valor_2);
+		printf("\nMenor valor: %f", valor_1);
+	} else {
+		printf("\nOs valores s�o iguais");
+		printf("\nValor: %f", valor_1);
+	}
+}
