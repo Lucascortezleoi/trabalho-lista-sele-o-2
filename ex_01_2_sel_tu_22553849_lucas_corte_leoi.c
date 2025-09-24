1e187799ea2904b807e40318c81fe65cebf6d0a0
Lucascortezleoi <lucasleoi2007@gmail.com>
2025-09-23 22:40:27 -0300

    Create ex_01_2_sel_tu_22553849_lucas_corte_leoi.c

---
+++ ex_01_2_sel_tu_22553849_lucas_corte_leoi.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <locale.h>
+
+void main(void) {
+	 setlocale(LC_ALL, "Portuguese");
+
+	int ano_nascimento, ano_atual, idade;
+	
+	printf("\nDigite seu ano de nascimento: ");
+	
+	scanf("%d", &ano_nascimento);
+	
+	printf("\nDigite o ano atual: ");
+	
+	scanf("%d", &ano_atual);
+	
+	idade = ano_atual - ano_nascimento;
+	
+	printf("\nSeu ano de nascimento �: %d", ano_nascimento);
+	printf("\nSua idade �: %d", idade);
+	
+	if(idade >= 16){
+		printf("\nA idade � %d anos e j� pode votar", idade);
+	} else {
+		printf("\nA idade � %d anos e n�o pode votar", idade);
+	}
+}
