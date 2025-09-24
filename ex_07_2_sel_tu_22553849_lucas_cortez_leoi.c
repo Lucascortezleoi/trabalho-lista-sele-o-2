1597591a5bbd27bd67c1586fb76a580bf20d316d
Lucascortezleoi <lucasleoi2007@gmail.com>
2025-09-23 22:48:25 -0300

    Create ex_07_2_sel_tu_22553849_lucas_cortez_leoi.c

---
+++ ex_07_2_sel_tu_22553849_lucas_cortez_leoi.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <locale.h>
+
+void main(void) {
+    setlocale(LC_ALL, "Portuguese");
+    
+    float nota1, nota2, media;
+    int peso1, peso2;
+    
+    printf("\nPrimeira nota: ");
+    scanf("%f", &nota1);
+    
+    printf("\nPeso da primeira nota: ");
+    scanf("%i", &peso1);
+    
+    printf("\nSegunda nota: ");
+    scanf("%f", &nota2);
+    
+    printf("\nPeso da segunda nota: ");
+    scanf("%i", &peso2);
+    
+    media = ((nota1 * peso1) + (nota2 * peso2)) / (peso1 + peso2);
+    
+    printf("\nMedia: %.2f ", media);
+    
+    if (media >=5) {
+    	printf("Aluno aprovado");
+	} else {
+		printf("Aluno reprovado");
+	}
+}
